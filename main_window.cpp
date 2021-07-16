f181d75b08d7ec19c48ef23061462f34b8eefdc4
lorewalker-chen <c52673687@163.com>
2021-07-16 23:50:47 +0800

    test by playing local video, successed

--- main_window.cpp
+++ main_window.cpp
@@ -1,15 +1,20 @@
 #include "main_window.h"
 #include "ui_main_window.h"
 
-MainWindow::MainWindow(QWidget *parent)
+MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    , ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    decoder_.reset(new Decoder("./1.mp4", 800, 600));
+    connect(decoder_.data(), &Decoder::GotImage, this, &MainWindow::GotImage);
+    decoder_->OpenVideo();
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::GotImage(const QImage& image) {
+    ui->label->setPixmap(QPixmap::fromImage(image));
 }
 
--- main_window.h
+++ main_window.h
@@ -1,21 +1,28 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#include "decoder.h"
 
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
+  public:
+    MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
+  private:
+    Ui::MainWindow* ui;
+
+    QScopedPointer<Decoder> decoder_;
+
+  private slots:
+    void GotImage(const QImage& image);
 };
 #endif // MAINWINDOW_H
